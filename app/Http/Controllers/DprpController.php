fdc53a5e8f3787eafbaac56648a99e245c2ba47d
YayaSuryana <yayasuryana542@gmail.com>
2023-07-03 06:35:49 +0700

    define fitur DPRP

---
+++ app/Http/Controllers/DprpController.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Dprp;
+use App\Models\Desa;
+use App\Models\Kecamatan;
+use App\Models\Kabupaten;
+use App\Models\Parpol;
+use Illuminate\Support\Facades\DB;
+
+class DprpController extends Controller
+{
+    public function getKecamatan(Request $request){
+        $kecamatan = Kecamatan::where("id_kabupaten", $request->kabID)
+        ->orderBy('kecamatan','asc')
+        ->pluck('kecamatan');
+        return response()->json($kecamatan);
+    }
+    
+    public function getDesa(Request $request){
+        $desa = Desa::where("id_kematan", $request->kecID)
+        ->orderBy('desa','asc')
+        ->pluck('desa');
+        return response()->json($desa);
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $kab = request('kabupaten');
+        $kec = request('kecamatan');
+        $des = request('desa');
+        $tps = request('tps');
+        $collection = Dprp::report()->get();
+
+        $partai = Dprp::select('partai', DB::raw('SUM(total) AS total_suara_partai'))
+                
+        ->when(request('tps'), function ($query) {
+            return $query->where('tps', request('tps'));
+        })
+        ->when(request('desa'), function ($query) {
+            return $query->where('desa', request('desa'));
+        })
+        ->when(request('kecamatan'), function ($query) {
+            return $query->where('kecamatan', request('kecamatan'));
+        })
+        ->when(request('kabupaten'), function ($query) {
+            return $query->where('kabupaten', request('kabupaten'));
+        })
+        ->when(request('partai'), function ($query){
+            return $query->where('partai', request('partai'))->havingRaw('SUM(total) > 0');
+        })
+        ->groupBy('partai')
+        ->pluck('total_suara_partai', 'partai')
+        ->mapWithKeys(function ($value, $key) {
+            return [$key => $value];
+        })
+        ->toArray();
+
+            $tpsVal = Dprp::select('tps', DB::raw('SUM(total) AS total_suara_tps'))
+
+            ->when(request('tps'), function ($query) {
+                return $query->where('tps', request('tps'));
+            })
+            ->when(request('desa'), function ($query) {
+                return $query->where('desa', request('desa'));
+            })
+            ->when(request('kecamatan'), function ($query) {
+                return $query->where('kecamatan', request('kecamatan'));
+            })
+            ->when(request('kabupaten'), function ($query) {
+                return $query->where('kabupaten', request('kabupaten'));
+            })
+            ->when(request('partai'), function ($query){
+                return $query->where('partai', request('partai'))->havingRaw('SUM(total) > 0');
+            })
+            ->groupBy('tps')
+            ->pluck('total_suara_tps', 'tps')
+            ->mapWithKeys(function ($value, $key) {
+                return [$key => $value];
+            })
+            ->toArray();
+        
+            
+            
+            
+            $desaVal = Dprp::select('desa', DB::raw('SUM(total) AS total_suara_desa'))
+        
+            ->when(request('tps'), function ($query) {
+                return $query->where('tps', request('tps'));
+            })
+            ->when(request('desa'), function ($query) {
+                return $query->where('desa', request('desa'));
+            })
+            ->when(request('kecamatan'), function ($query) {
+                return $query->where('kecamatan', request('kecamatan'));
+            })
+            ->when(request('kabupaten'), function ($query) {
+                return $query->where('kabupaten', request('kabupaten'));
+            })
+            ->when(request('partai'), function ($query){
+                return $query->where('partai', request('partai'))->havingRaw('SUM(total) > 0');
+            })
+            ->groupBy('desa')
+            ->pluck('total_suara_desa', 'desa')
+            ->mapWithKeys(function ($value, $key) {
+                return [$key => $value];
+            })
+            ->toArray();
+
+
+            $kecamatanVal = Dprp::select('kecamatan', DB::raw('SUM(total) AS total_suara_kecamatan'))
+        
+            ->when(request('tps'), function ($query) {
+                return $query->where('tps', request('tps'));
+            })
+            ->when(request('desa'), function ($query) {
+                return $query->where('desa', request('desa'));
+            })
+            ->when(request('kecamatan'), function ($query) {
+                return $query->where('kecamatan', request('kecamatan'));
+            })
+            ->when(request('kabupaten'), function ($query) {
+                return $query->where('kabupaten', request('kabupaten'));
+            })
+            ->when(request('partai'), function ($query){
+                return $query->where('partai', request('partai'))->havingRaw('SUM(total) > 0');
+            })
+            ->groupBy('kecamatan')
+            ->pluck('total_suara_kecamatan', 'kecamatan')
+            ->mapWithKeys(function ($value, $key) {
+                return [$key => $value];
+            })
+            ->toArray();
+            
+            $kabupatenVal = Dprp::select('kabupaten', DB::raw('SUM(total) AS total_suara_kabupaten'))
+        
+            ->when(request('tps'), function ($query) {
+                return $query->where('tps', request('tps'));
+            })
+            ->when(request('desa'), function ($query) {
+                return $query->where('desa', request('desa'));
+            })
+            ->when(request('kecamatan'), function ($query) {
+                return $query->where('kecamatan', request('kecamatan'));
+            })
+            ->when(request('kabupaten'), function ($query) {
+                return $query->where('kabupaten', request('kabupaten'));
+            })
+            ->when(request('partai'), function ($query){
+                return $query->where('partai', request('partai'))->havingRaw('SUM(total) > 0');
+            })
+            ->groupBy('kabupaten')
+            ->pluck('total_suara_kabupaten', 'kabupaten')
+            ->mapWithKeys(function ($value, $key) {
+                return [$key => $value];
+            })
+            ->toArray();
+            
+            $kabupaten = Kabupaten::all();
+            $parpol = Parpol::all();
+
+        return view('welcome', compact('collection','partai','tpsVal','desaVal','kecamatanVal','kabupatenVal','kabupaten','parpol','kab','kec','des','tps'));
+    }
+}
--- resources/views/dprp.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('welcome')
-@section('content')
-    <h1>DPRP</h1>
-    <h4>Maaf kontent belum tersedia</h4>
-@endsection
